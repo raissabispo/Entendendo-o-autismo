f641c31c2a1ff95b15ec2c853f8edd8bbcee225f
Raissa Vitória <123013855+raissabispo@users.noreply.github.com>
2024-09-04 14:39:48 -0300

    Add files via upload

---
+++ livros.js
@@ -0,0 +1,79 @@
+let livros = [
+  {
+    titulo: "Harry Potter e a Pedra Filosofal",
+    autor: "J.K. Rowling",
+    genero: "Fantasia",
+    descricao: "Um garoto órfão descobre que é um bruxo e embarca em uma aventura mágica em Hogwarts.",
+    link: "https://harrypotter.fandom.com/pt-br/wiki/Harry_Potter_e_a_Pedra_Filosofal"
+  },
+  {
+    titulo: "Percy Jackson e o Ladrão de Raios",
+    autor: "Rick Riordan",
+    genero: "Fantasia",
+    descricao: "Um garoto descobre que é um semideus e precisa salvar o mundo de uma guerra entre os deuses do Olimpo.",
+    link: "https://pt.wikipedia.org/wiki/Percy_Jackson_e_o_Ladr%C3%A3o_de_Raios"
+  },
+  {
+    titulo: "As Crônicas de Nárnia: O Leão, a Feiticeira e o Guarda-Roupa",
+    autor: "C.S. Lewis",
+    genero: "Fantasia",
+    descricao: "Quatro irmãos encontram um mundo mágico através de um guarda-roupa e lutam contra a Feiticeira Branca.",
+    link: "https://pt.wikipedia.org/wiki/As_Cr%C3%B3nicas_de_N%C3%Ania:_O_Le%C3%Aao,_a_Feiticeira_e_o_Guarda-Roupa"
+  },
+  {
+    titulo: "O Senhor dos Anéis: A Sociedade do Anel",
+    autor: "J.R.R. Tolkien",
+    genero: "Fantasia",
+    descricao: "Um hobbit embarca em uma jornada épica para destruir o Um Anel e salvar a Terra Média.",
+    link: "https://pt.wikipedia.org/wiki/O_Senhor_dos_An%C3%A9is:_A_Sociedade_do_Anel"
+  },
+  {
+    titulo: "Orgulho e Preconceito",
+    autor: "Jane Austen",
+    genero: "Romance",
+    descricao: "Uma história de amor e preconceito entre Elizabeth Bennet e Mr. Darcy.",
+    link: "https://pt.wikipedia.org/wiki.org/Orgulho_e_Preconceito"
+  },
+  {
+    titulo: "O Pequeno Príncipe",
+    autor: "Antoine de Saint-Exupéry",
+    genero: "Ficção",
+    descricao: "Um piloto encontra um pequeno príncipe em um deserto e juntos exploram temas como amizade, amor e a importância de ver o mundo com os olhos da criança.",
+    link: "https://pt.wikipedia.org/wiki/O_Pequeno_Pr%C3%ADncipe"
+  },
+  {
+    titulo: "Dom Quixote",
+    autor: "Miguel de Cervantes",
+    genero: "Romance",
+    descricao: "Um hidalgo sonha em ser um cavaleiro andante e embarca em aventuras hilárias e tragicômicas.",
+    link: "https://pt.wikipedia.org/wiki/Don_Quixote"
+  },
+  {
+    titulo: "Guerra e Paz",
+    autor: "Leon Tolstói",
+    genero: "Romance Histórico",
+    descricao: "Um épico que retrata a vida da nobreza russa durante as Guerras Napoleônicas, explorando temas como amor, guerra e a condição humana.",
+    link: "https://pt.wikipedia.org/wiki/Guerra_e_Paz"
+  },
+  {
+    titulo: "Cem Anos de Solidão",
+    autor: "Gabriel García Márquez",
+    genero: "Realismo Mágico",
+    descricao: "Uma saga familiar que narra a história da cidade fictícia de Macondo ao longo de várias gerações.",
+    link: "https://pt.wikipedia.org/wiki/Cem_Anos_de_Solid%C3%A3o"
+  },
+  {
+    titulo: "Moby Dick",
+    autor: "Herman Melville",
+    genero: "Aventura",
+    descricao: "A história obsessiva de um capitão em busca de uma baleia branca, Moby Dick.",
+    link: "https://pt.wikipedia.org/wiki/Moby_Dick"
+  },
+  {
+    titulo: "1984",
+    autor: "George Orwell",
+    genero: "Distopia",
+    descricao: "Um romance que retrata um futuro distópico onde um governo totalitário controla a vida de todos os cidadãos.",
+    link: "https://pt.wikipedia.org/wiki/1984_(livro)"
+  }
+];
